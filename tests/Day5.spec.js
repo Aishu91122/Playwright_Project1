21226715a2c77db0a8a91cadf2fdd878be0490fc
Aishu91122 <ice.sanju@gmail.com>
2026-07-11 14:15:51 +0530

    Assertions Added

--- tests/Day5.spec.js
+++ tests/Day5.spec.js
@@ -3,4 +3,7 @@ import {test, chromium} from '@playwright/test'
 test ("Launch Browser", async ({page})=> {
 page.goto("https://www.makemytrip.com/");
 console.log(await page.title())
+console.log("This line is to validate Title");
+
+
 })
